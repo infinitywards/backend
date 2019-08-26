892cc823ada706d3cdfda425fc1999ed200d64e9
infinitywards <54397109+infinitywards@users.noreply.github.com>
2019-08-26 13:30:03 +0530

    Update sendSms.js

--- app/services/sendSms.js
+++ app/services/sendSms.js
@@ -9,7 +9,7 @@ function sendSms(msg,number){
     client.messages.create({
         body:msg,
         to: number,  // Text this number
-        from: '+12035909565' // From a valid Twilio number
+        from: '+12022171993' // From a valid Twilio number
     })
     .then((message) => console.log("message sent :",message.sid));
 
